3cacfdf4c4e4377cacbe2c14922a19e862cf1eaf
Edward Tsai <asdeq20062@gmail.com>
2018-04-29 15:12:59 +0800

    make it better

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+users = [];
+connections = [];
+
+server.listen(8080);
+console.log('Server running...')
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname +  "/index.html")
+});
+
+io.sockets.on('connection', function (socket){
+	connections.push(socket);
+	var address = String(socket.request.connection.remoteAddress);
+	console.log('New connection from ' + address);
+
+	// Disconnect
+	socket.on('disconnect', function(){
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('Disconnected: %s sockets disconnected', connections.length)
+	});
+
+});
