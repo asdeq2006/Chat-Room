0a43e64d81a6b7136a850ff4b6167fc135b6e459
Edward Tsai <asdeq20062@gmail.com>
2018-04-29 19:01:34 +0800

    make it better

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ io.sockets.on('connection', function (socket){
 
 	//Send Message
 	socket.on('send message',function(data){
-		io.sockets.emit('new message',{msg: data});
+		io.sockets.emit('new message',{msg: data, user: socket.username});
 	});
 
 	// New User
